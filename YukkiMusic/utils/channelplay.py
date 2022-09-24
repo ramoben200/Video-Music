5e9979024a6e48dd3b9f2f57040fa67a588b8d12
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 14:30:49 +0300

    Update channelplay.py

--- YukkiMusic/utils/channelplay.py
+++ YukkiMusic/utils/channelplay.py
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by ramoben200@Github, < https://github.com/ramoben200 >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ramoben200/BallasMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ramoben200/BallasMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_cmode
+from BallasMusic import app
+from BallasMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
