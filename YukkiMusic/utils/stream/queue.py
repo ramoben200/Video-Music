a7867e9a03bfc38833b536478b938c1bcff1d3dd
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 14:49:40 +0300

    Update queue.py

--- YukkiMusic/utils/stream/queue.py
+++ YukkiMusic/utils/stream/queue.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by ramoben200@Github, < https://github.com/ramoben200 >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ramoben200/BallasMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ramoben200/BallasMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -11,7 +11,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from YukkiMusic.misc import db
+from BallasMusic.misc import db
 
 
 async def put_queue(
