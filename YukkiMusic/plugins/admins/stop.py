bebf1dda9e2e2ec42fc31b2b7f15709306a1b97e
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 17:19:30 +0300

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by ramoben200@Github, < https://github.com/ramoben200 >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ramoben200/BallasMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ramoben200/BallasMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import set_loop
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from BallasMusic import app
+from BallasMusic.core.call import Ballas
+from BallasMusic.utils.database import set_loop
+from BallasMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Ballas.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
