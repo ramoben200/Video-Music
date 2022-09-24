6bc18c4adf2f7bfb8ef48f63dda6e24fbd11afa6
Ramo <112237128+ramoben200@users.noreply.github.com>
2022-09-24 18:41:24 +0300

    Update heroku.py

--- YukkiMusic/plugins/sudo/heroku.py
+++ YukkiMusic/plugins/sudo/heroku.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by ramoben200@Github, < https://github.com/ramoben200 >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ramoben200/BallasMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ramoben200/BallasMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -24,13 +24,13 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import HAPP, SUDOERS, XCB
-from YukkiMusic.utils.database import (get_active_chats,
+from BallasMusic import app
+from BallasMusic.misc import HAPP, SUDOERS, XCB
+from BallasMusic.utils.database import (get_active_chats,
                                        remove_active_chat,
                                        remove_active_video_chat)
-from YukkiMusic.utils.decorators.language import language
-from YukkiMusic.utils.pastebin import Yukkibin
+from BallasMusic.utils.decorators.language import language
+from BallasMusic.utils.pastebin import Ballasbin
 
 # Commands
 GETLOG_COMMAND = get_command("GETLOG_COMMAND")
